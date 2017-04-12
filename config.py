b943fdb9137dd0f4a79045b9351a266e5216977d
Eric Wieser <wieser.eric@gmail.com>
2017-04-12 12:30:29 +0100

    Prompt for user information for missing accounts

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
 #! python3
 import keyrings.alt
+from pprint import pprint
 
 from interfaces import *
 
@@ -14,5 +15,11 @@ cred_store.file_path = 'cred.cfg'
 if __name__ == '__main__':
 	for a in accounts:
 		print(a.name)
-		print(vars(a))
+		pprint(vars(a))
 		print()
+
+	cred_store.keyring_key
+	for a in accounts:
+		a.auth_from_store(cred_store)
+		print(a.name)
+		pprint(vars(a))
